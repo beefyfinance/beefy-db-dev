899f79c1c735aef0bca54e49da0b147ca40a0d85
superbeefyboy <superbeefyboy@pm.me>
2021-07-13 02:22:17 -0300

    wip

--- src/fetch.js
+++ src/fetch.js
@@ -1,25 +1,51 @@
 const axios = require('axios');
+
+const { FETCH_INTERVAL } = require('./cfg');
 const { log } = require('./log');
 
 const BASE_URL = "https://api.beefy.finance";
 
-async function fetchTvl () {
-  log.info(`fetching tvl`);
-  return axios.get(`${BASE_URL}/tvl`);
+async function fetch () {
+  try {
+    const t = Math.floor(Date.now() / (60 * 60 * 1000));
+    const [apy, price, tvl] = await Promise.all([
+      fetchApy(t),
+      fetchPrice(t),
+      fetchTvl(t), 
+    ]);
+  
+    // TODO: persist data
+  
+    log.debug('TVL');
+    console.log(price);
+    running = false;
+  
+    setTimeout(fetch, FETCH_INTERVAL);
+
+  } catch (err) {
+    log.error(err);
+    setTimeout(fetch, ERROR_INTERVAL);
+  }
 }
 
-async function fetchApy () {
+async function fetchApy (t) {
   log.info(`fetching apy`);
-  return axios.get(`${BASE_URL}/apy/breakdown`);
+  return axios.get(`${BASE_URL}/apy/breakdown?_${t}`);
 }
 
-async function fetchPrice () {
+async function fetchPrice (t) {
   log.info(`fetching price`);
-  return axios.get(`${BASE_URL}/prices`);
+  return axios.get(`${BASE_URL}/prices?_${t}`);
 }
 
-module.exports = { 
-  fetchTvl,
+async function fetchTvl (t) {
+  log.info(`fetching tvl`);
+  return axios.get(`${BASE_URL}/tvl?_${t}`);
+}
+
+module.exports = {
+  fetch,
   fetchApy,
-  fetchPrice
+  fetchPrice,
+  fetchTvl,
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-const { existsSync, writeFileSync, unlinkSync, readFileSync } = require('fs');
+const { existsSync, writeFileSync, unlinkSync } = require('fs');
 
 const { LOCK_FILE } = require('./constants');
-const { FETCH_INTERVAL, ERROR_INTERVAL } = require('./cfg');
-
 const { log } = require('./log');
 const { sleep } = require('./utils');
-const { fetchTvl, fetchApy, fetchPrice } = require('./fetch');
-
-let running = true;
+const { fetch } = require('./fetch');
 
 async function main () {
   log.info('beefy-db start');
@@ -22,24 +18,15 @@ async function main () {
     writeFileSync(LOCK_FILE, Date.now().toString());
     log.debug('db locked');
 
-    // TODO: launch this as a thread
-    while (running) {
-      const [tvl, apy, price] = Promise.all([
-        fetchTvl(), fetchApy(), fetchPrice()
-      ]);
+    setTimeout(fetch, 1000);
 
-      // TODO: store data
-      log.log('TVL');
-      log.log(tvl);
-      running = false;
-
-      await sleep(FETCH_INTERVAL);
-    }
+    // TODO: start listener
+    await sleep(10 * 60 * 1000)
     
   } catch (err) {
     log.error(err);
-    await sleep(ERROR_INTERVAL);
-  }  
+    throw err;
+  }
 
   if (existsSync(LOCK_FILE)) {
     log.debug('unlocking db');
