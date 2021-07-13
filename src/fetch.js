3007c1dd17ff97e2a19a73cf444d1c567a5b3044
superbeefyboy <superbeefyboy@pm.me>
2021-07-12 23:18:09 -0300

    fetch

--- src/fetch.js
+++ src/fetch.js
@@ -1,21 +1,21 @@
+const axios = require('axios');
 const { log } = require('./log');
+
+const BASE_URL = "https://api.beefy.finance";
 
 async function fetchTvl () {
   log.info(`fetching tvl`);
-  // Fetch TVL  
-  return {};
+  return axios.get(`${BASE_URL}/tvl`);
 }
 
 async function fetchApy () {
   log.info(`fetching apy`);
-  // Fetch TVL  
-  return {};
+  return axios.get(`${BASE_URL}/apy/breakdown`);
 }
 
 async function fetchPrice () {
   log.info(`fetching price`);
-  // Fetch TVL  
-  return {};
+  return axios.get(`${BASE_URL}/prices`);
 }
 
 module.exports = { 
