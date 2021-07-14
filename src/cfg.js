07ab568869a19eb775c876b0c6b2361158f5f80f
superbeefyboy <superbeefyboy@pm.me>
2021-07-14 07:48:53 -0300

    simplified cfg

--- src/cfg.js
+++ src/cfg.js
@@ -1,9 +1,7 @@
 const UPDATE_INTERVAL = process.env.UPDATE_INTERVAL || 5 * 60 * 1000;
-const ERROR_INTERVAL = process.env.ERROR_INTERVAL || 60 * 1000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 module.exports = {
   UPDATE_INTERVAL,
-  ERROR_INTERVAL,
   DATABASE_URL
 }
