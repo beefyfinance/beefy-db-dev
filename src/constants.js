10726fb9bcb4829bbe73a6fcdacd0fdda65bd368
superbeefyboy <superbeefyboy@pm.me>
2021-07-14 07:18:40 -0300

    wip

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,12 @@
 const LOCK_FILE = './db/lock';
+const SNAP_FILE = './db/snapshot';
 
 const HOUR = 60 * 60;
 const HOUR_IN_MILLIS = HOUR * 1000;
 
 module.exports = {
   LOCK_FILE,
+  SNAP_FILE,
   HOUR,
   HOUR_IN_MILLIS
 }
--- src/core.js
+++ src/core.js
@@ -1,5 +1,7 @@
-const { HOUR, HOUR_IN_MILLIS} = require('./constants');
-const { UPDATE_INTERVAL, ERROR_INTERVAL } = require('./cfg');
+const { existsSync, writeFileSync, readFileSync } = require('fs');
+
+const { HOUR, HOUR_IN_MILLIS, SNAP_FILE } = require('./constants');
+const { UPDATE_INTERVAL } = require('./cfg');
 
 const { log } = require('./log');
 const db = require('./db');
@@ -14,27 +16,50 @@ async function init () {
 
 async function update () {
   log.info(`updating snapshot`);
+  
   try {
     const t = Math.floor(Date.now() / HOUR_IN_MILLIS) * HOUR;
-    const [apy, price, tvl] = await Promise.all([
-      fetchApy(t),
-      fetchPrice(t),
-      fetchTvl(t), 
-    ]);
-
-    await Promise.all([
-      db.insert("apys", transformApy(apy.data || {}, t)),
-      db.insert("prices", transformPrice(price.data || {}, t)),
-      db.insert("tvls", transformTvl(tvl.data || {}, t)), 
-    ]);
-
-    setTimeout(update, UPDATE_INTERVAL);
-
+    if (!hasSnapshot(t)){
+      await snapshot(t);
+    }
   } catch (err) {
     log.error(err);
-    setTimeout(update, ERROR_INTERVAL);
   }
+  
+  setTimeout(update, UPDATE_INTERVAL);
   log.info(`updated snapshot`);
+}
+
+function hasSnapshot (t) {
+  log.debug(`checking last snapshot`);
+
+  if (!existsSync(SNAP_FILE)) {
+    log.debug(`snapshot not found`);
+    return false;
+  }
+
+  log.debug(`snapshot found`);
+  const last = Number(readFileSync(SNAP_FILE));
+  console.log(last, last === t);
+
+  return last === t;  
+
+}
+
+async function snapshot (t) {
+  const [apy, price, tvl] = await Promise.all([
+    fetchApy(t),
+    fetchPrice(t),
+    fetchTvl(t), 
+  ]);
+
+  await Promise.all([
+    db.insert("apys", transformApy(apy.data || {}, t)),
+    db.insert("prices", transformPrice(price.data || {}, t)),
+    db.insert("tvls", transformTvl(tvl.data || {}, t)), 
+  ]);
+
+  writeFileSync(SNAP_FILE, t.toString());
 }
 
 module.exports = {
