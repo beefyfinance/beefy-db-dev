b7a71a5050bbc926a123702eea9201551ea950cb
superbeefyboy <superbeefyboy@pm.me>
2021-08-07 17:34:19 -0300

    bulk request apy data

--- src/api/actions.js
+++ src/api/actions.js
@@ -1,7 +1,11 @@
 const db = require('../data/db');
 
 // FIXME: make this dynamic. name can safely be ignored, since you always know what you are asking for
+const DAY_IN_SECS = 24 * 60 * 60;
 const WEEK_IN_SECS = 7 * 24 * 60 * 60;
+
+// FIXME: used to cap how many records we can get in bulk. 20k should be more than enough for now
+const BULK_LIMIT = 20000;
 
 function parseFilter (q) {
   const filter = {}
@@ -36,6 +40,34 @@ async function apy (ctx) {
   });
 }
 
+async function bulk (ctx) {
+  const to = Math.floor(Date.now() / (DAY_IN_SECS * 1000)) * DAY_IN_SECS;
+  const from = to - WEEK_IN_SECS;
+
+  const res = await db.query({
+    table: "apys",
+    filter: {
+      from: from,
+      to: to,
+      period: 'day',
+      order: 'ASC',
+      limit: BULK_LIMIT
+    }
+  });
+
+  const group = {};
+  for (let i = 0; i < res.rows.length; i++) {
+    const r = res.rows[i];
+    if (group[r.name]){
+      group[r.name].push(r.v.toFixed(2));
+    } else {
+      group[r.name] = [r.v.toFixed(2)];
+    }
+  }
+  
+  ctx.body = group;
+}
+
 async function price (ctx) {
   ctx.body = await execQuery({ 
     ctx,
@@ -52,6 +84,7 @@ async function tvl (ctx) {
 
 module.exports = {
   apy,
+  bulk,
   price,
   tvl
 };
--- src/api/router.js
+++ src/api/router.js
@@ -4,6 +4,7 @@ const router = new Router();
 const actions = require('./actions'); 
 const noop = require('./noop');
 
+router.get('/bulk', actions.bulk);
 router.get('/apy', actions.apy);
 router.get('/price', actions.price);
 router.get('/tvl', actions.tvl);
